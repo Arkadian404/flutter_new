45c29d1da563eac6e7f0de19b7c77005fa33783d
ark <phutv1990@gmail.com>
2022-08-25 20:33:58 +0700

    First step

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatelessWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Center(
+          child: Text('Widget Playground'),
+        ));
+  }
+}
