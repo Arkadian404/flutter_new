4a56d2fde657f7089688c29a165388f2db1a9ea8
ark <phutv1990@gmail.com>
2022-08-30 23:17:04 +0700

    Create a new chart and fix some bugs!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import "./widgets/user_transactions.dart";
+import 'package:flutter_complete_guide/widgets/new_transactions.dart';
+import 'package:flutter_complete_guide/widgets/transaction_list.dart';
+import "./models/transaction.dart";
+import './widgets/chart.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,38 +13,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
+      theme: ThemeData(primaryColor: Colors.purple, accentColor: Colors.amber),
       home: MyHomepage(),
     );
   }
 }
 
-// ignore: must_be_immutable
-class MyHomepage extends StatelessWidget {
-  MyHomepage({Key? key}) : super(key: key);
-  final inputAmount = TextEditingController();
-  final inputTitle = TextEditingController();
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(id: 't1', title: 'New shoes', amount: 60, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Weekly groceries', amount: 50, date: DateTime.now()),
+  ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            child: NewTransaction(_addNewTransaction),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: const Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           //mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              color: Color.fromARGB(255, 15, 82, 136),
-              child: Card(
-                child: Text("CHART!"),
-              ),
-            ),
-            UserTransaction(),
+            Chart(_recentTransactions),
+            TransactionList(_userTransactions),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,11 +1,28 @@
 import "package:flutter/material.dart";
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
+
+  // ignore: use_key_in_widget_constructors
+  const NewTransaction(this.addTx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final inputAmount = TextEditingController();
+
   final inputTitle = TextEditingController();
 
-  NewTransaction(this.addTx);
+  void submitData() {
+    final enteredTitle = inputTitle.text;
+    final enteredAmount = double.parse(inputAmount.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) return;
+
+    widget.addTx(enteredTitle, enteredAmount);
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +35,16 @@ class NewTransaction extends StatelessWidget {
               TextField(
                 decoration: InputDecoration(labelText: "Title"),
                 controller: inputTitle,
+                onSubmitted: (_) => submitData(),
               ),
               TextField(
                 decoration: InputDecoration(labelText: "Amount"),
                 controller: inputAmount,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => submitData(),
               ),
               FlatButton(
-                onPressed: () {
-                  addTx(inputTitle.text, double.parse(inputAmount.text));
-                },
+                onPressed: submitData,
                 child: Text("Add transaction"),
                 textColor: Colors.purple,
               )
